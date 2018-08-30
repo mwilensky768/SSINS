8f660f1e37ad8886558bdd119c9ea23e6929334d
Mike Wilensky <mjw768@uw.edu>
2018-08-30 15:41:03 -0700

    Added docstrings. Made the tests pass again (locally, do your worst, TRAVIS).

--- SSINS/__init__.py
+++ SSINS/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
+"""
+init file for SSINS
+"""
+
 from .event_stat import *
 from .incoherent_noise_spectrum import *
 from .match_filter import *
