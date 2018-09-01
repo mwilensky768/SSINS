396288051608c33b4e0721dde145da9ca1900bcf
Mike Wilensky <mjw768@uw.edu>
2018-08-31 19:31:18 -0700

    Various fixes to ss.write()

--- SSINS/sky_subtract.py
+++ SSINS/sky_subtract.py
@@ -211,6 +211,8 @@ class SS:
             UV.flag_array = np.zeros([UV.Ntimes, UV.Nbls, UV.Nspws, UV.Nfreqs,
                                       UV.Npols], dtype=bool)
             # This actually does not invert properly but I think it's the best way
-            UV.flag_array[i][self.UV.data_arra.mask[i]] = 1
+            UV.flag_array[i][self.UV.data_array.mask[i]] = 1
             UV.flag_array[i + 1][self.UV.data_array.mask[i]] = 1
+        UV.flag_array = UV.flag_array.reshape([UV.Nblts, UV.Nspws, UV.Nfreqs,
+                                               UV.Npols])
         getattr(UV, 'write_%s' % file_type_out)(outpath)
