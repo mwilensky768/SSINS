34939c440d4b1f4cefe1c3f0be5654f3936a1dee
Mike Wilensky <mjw768@uw.edu>
2018-08-31 19:22:08 -0700

    Changed write function and added docstrings

--- SSINS/match_filter.py
+++ SSINS/match_filter.py
@@ -1,3 +1,7 @@
+"""
+Match Filter class
+"""
+
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
@@ -7,8 +11,52 @@ from scipy.special import erfc
 
 class MF:
 
+    """
+    Defines the Match Filter (MF) class. It requires an INS object to be passed
+    to it in order to operate on it.
+    """
+
     def __init__(self, INS, sig_thresh=None, shape_dict={}, N_thresh=0, alpha=None,
                  point=True, streak=True):
+
+        """
+        init function for match filter (MF) class. This function assigns some
+        basic attributes and converts the frequencies in the shape_dict to
+        channel numbers (see shape_dict description below).
+
+        Parameters: sig_thresh: The outlier threshold in units of the estimated
+                                width of the thermal noise. A reasonable
+                                choice can be calculated from the size of the
+                                data set, by asking for the value beyond which
+                                less than 1 count is expected for a data set
+                                which is just noise (perfectly clean).
+
+                    shape_dict: This is used in apply_match_test(). The keys are
+                                used strictly internally, so they can be named
+                                arbitrarily. The values should be the upper and
+                                lower frequency limits of the expected RFI
+                                (in hz).
+
+                    N_thresh:   Used in apply_samp_thresh_test(). If the number
+                                of unflagged samples for a shape or channel is
+                                less than N_thresh, then the entire observation
+                                will be flagged for that shape or channel.
+
+                    alpha:      Used in apply_chi_square_test(). If a shape or
+                                channel's histogram gives a chi-square of p-value
+                                less than or equal to alpha, the entire
+                                observation will be flagged for that shape
+                                or channel.
+
+                    point:      If this is true, single-point outliers will be
+                                searched for along with whatever shapes are
+                                passed in the shape_dict.
+
+                    streak:     If this is true, broadband RFI which is not
+                                band-limited will be sought along with whatever
+                                shapes are passed in the shape_dict.
+        """
+
         self.INS = INS
         self.shape_dict = shape_dict
         self.N_thresh = N_thresh
@@ -21,6 +69,13 @@ class MF:
         self.slice_dict = self.shape_slicer(point, streak)
 
     def shape_slicer(self, point, streak):
+
+        """
+        This function converts the frequency information in the shape_dict
+        attribute to slice objects for the channel numbers of the spectrum.
+        The point and streak shapes require special slices.
+        """
+
         slice_dict = {}
         for shape in self.shape_dict:
             if min(self.INS.freq_array[0, :]) < min(self.shape_dict[shape]) or \
@@ -35,6 +90,13 @@ class MF:
         return(slice_dict)
 
     def match_test(self):
+
+        """
+        The primary test that the filter is used with. The greatest outlier in
+        each shape is put forth and a champion among them is chosen. The time,
+        frequencies, and outlier statistic of this champion is returned to the
+        stack.
+        """
 
         R_max = -np.inf
         t_max = None
@@ -60,6 +122,11 @@ class MF:
 
     def chisq_test(self):
 
+        """
+        A test to measure the chi-square of the binned shapes and channels
+        relative to standard normal noise (the null hypothesis of the filter).
+        """
+
         p_min = 1
         shape_min = None
         for shape in self.slice_dict:
@@ -83,6 +150,13 @@ class MF:
 
     def apply_match_test(self):
 
+        """
+        Where match_test() is implemented. The champion from match_test() is
+        flagged if its outlier statistic is greater than sig_thresh, and the
+        mean-subtracted spectrum is recalculated. This repeats
+        until there are no more outliers greater than sig_thresh.
+        """
+
         count = 1
         # Set these attributes to list form so that append method works
         for attr in ['match_events', 'match_hists']:
@@ -102,6 +176,12 @@ class MF:
         self.INS.counts, self.INS.bins, self.INS.sig_thresh = self.INS.hist_make(sig_thresh=self.sig_thresh)
 
     def apply_chisq_test(self):
+        """
+        Calculates the p-value of each shape and channel using chisq_test().
+        Should the p-value of a shape or channel be less than the significance
+        threshold, alpha, the entire observation will be flagged for that shape
+        or channel.
+        """
         # Set these attributes to list so that append method works
         for attr in ['chisq_hists', 'chisq_events']:
             if type(getattr(self.INS, attr)) is not list:
@@ -127,6 +207,13 @@ class MF:
         self.INS.counts, self.INS.bins, self.INS.sig_thresh = self.INS.hist_make(sig_thresh=self.sig_thresh)
 
     def apply_samp_thresh_test(self):
+        """
+        A quick test to see if any channels are flagged beyond the tolerance
+        described by N_thresh. The goal of this test is to find channels which
+        are so thoroughly flagged that their statistics can no longer be trusted,
+        in which case it is likely that the entire observation for that channel
+        is contaminated, and so that channel is entirely flagged.
+        """
 
         assert self.N_thresh < self.INS.data.shape[0], 'N_thresh is greater than the number of times. This would result in flagging the entire observation regardless of content.'
         ind = np.where(np.count_nonzero(np.logical_not(self.INS.data.mask), axis=0) < self.N_thresh)[:-1]
