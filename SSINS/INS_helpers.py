e97bb5175faa9bc73a7f2387c02b48a9d59a6084
Mike Wilensky <mjw768@uw.edu>
2018-10-23 16:57:26 +1100

    Eliminated need for freq_sequence keyword

--- SSINS/INS_helpers.py
+++ SSINS/INS_helpers.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function
 from SSINS import util, INS
 
 
-def INS_concat(INS_sequence, axis, freq_sequence=None, metadata_kwargs={}):
+def INS_concat(INS_sequence, axis, metadata_kwargs={}):
     """
     This function is used to concatenate spectra. For instance, if polarizations
     are in separate files or if different parts of the observing band are in
@@ -17,7 +17,7 @@ def INS_concat(INS_sequence, axis, freq_sequence=None, metadata_kwargs={}):
     Nbls = np.concatenate([ins.Nbls for ins in INS_sequence], axis=axis)
     # This is the frequency axis
     if axis is 2:
-        freq_array = np.concatenate(freq_sequence, axis=1)
+        freq_array = np.concatenate([ins.freq_array for ins in INS_sequence], axis=1)
     else:
         freq_array = INS_sequence[0].freq_array
     ins = INS(data=data, Nbls=Nbls, freq_array=freq_array, **metadata_kwargs)
