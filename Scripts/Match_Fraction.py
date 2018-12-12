d2afabcbf5338626802cfe6e90a20091f9ccbd78
Mike Wilensky <mjw768@uw.edu>
2018-12-12 15:45:23 -0800

    Added match_fraction scripts

--- Scripts/Match_Fraction.py
+++ Scripts/Match_Fraction.py
@@ -1,12 +1,13 @@
-from __future__ import divide, absolute_import, print_function
+from __future__ import division, absolute_import, print_function
 
 import numpy as np
-from SSINS import util
+from SSINS import util, plot_lib
 import glob
-from SSINS import plot_lib as pl
 from matplotlib import cm, colors
 import matplotlib.pyplot as plt
 import argparse
+import pickle
+import os
 
 """
 Read in some pre-filtered noise spectra and make a scatter plot of their RFI
@@ -14,75 +15,91 @@ occupancies as a function of LST
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument('match_events_inpath', help='The input base directory')
-parser.add_argument('meta_inpath', help='The base directory with the time arrays')
-parser.add_argument('outpath', help='The target filename for the plot, should end in a valid matplotlib filetype')
-parser.add_argument('chan_min', type=int)
-parser.add_argument('chan_max', type=int)
-
+parser.add_argument('occ_dict', help='A pickled python dictionary of flagging occupancies')
+parser.add_argument('times_dir', help='The directory where the LST and JD are kept')
+parser.add_argument('shape', help='The shape to make the plots for')
+parser.add_argument('sig_thresh', type=float, help='The significance threshold to plot for')
+parser.add_argument('outdir', help='The output directory')
+parser.add_argument('-g', help='A boolean array of frequency channels to keep')
+parser.add_argument('--vmax', type=float, help='Clip the colorbar')
 args = parser.parse_args()
 
+if args.g is not None:
+    good_freqs = np.load(args.g)
 
-time_arrs = glob.glob('%s/*time*' % (args.meta_inpath))
-lst_arrs = glob.glob('%s/*lst*' % (args.meta_inpath))
-freq_arrs = glob.glob('%s/*freq_array*' % (args.meta_inpath))
-L2 = len('%s/' % (args.meta_inpath))
-L1 = len('%s/' % (args.match_events_inpath))
-match_event_arrs = glob.glob('%s/*match_events*' % args.match_events_inpath)
-obslist = [path[L1:L1 + 10] for path in match_event_arrs]
-master_obslist = [path[L2:L2 + 10] for path in time_arrs]
-for dat in [time_arrs, lst_arrs, match_event_arrs, freq_arrs]:
-    dat.sort()
+if not os.path.exists(args.outdir):
+    os.makedirs(args.outdir)
 
-JD = []
-lst = []
-match_event_frac = []
-max_JD_len = 0
-for i, obs in enumerate(master_obslist):
-    JD_arr = np.load(time_arrs[i])
-    if len(JD_arr) > max_JD_len:
-        max_JD_len = len(JD_arr)
-    JD.append(JD_arr[0])
-    lst_rad = np.load(lst_arrs[i])[0]
-    if lst_rad > np.pi:
-        lst_rad -= 2 * np.pi
-    lst.append(23.9345 / (2 * np.pi) * lst_rad)
-    Nfreqs = len(np.load(freq_arrs[i])[0])
-    if obs in obslist:
-        k = np.where(np.array(obslist) == obs)[0][0]
-        match = np.load(match_event_arrs[k])
-        event_frac = util.event_fraction(match, Nfreqs, len(JD_arr) - 1)
-        if (args.chan_min, args.chan_max) in event_frac:
-            match_event_frac.append(event_frac[(args.chan_min, args.chan_max)])
+occ_dict = pickle.load(open(args.occ_dict, 'rb'))[args.sig_thresh][args.shape]
+print(len(occ_dict.keys()))
+
+scat_dat = np.zeros((len(occ_dict.keys()), 3))
+missing_obs = []
+
+for i, obs in enumerate(occ_dict):
+    x_path = '%s/%s_lst_arr.npy' % (args.times_dir, obs)
+    y_path = '%s/%s_times_arr.npy' % (args.times_dir, obs)
+    if os.path.exists(x_path) and os.path.exists(y_path):
+        x = np.load(x_path)[0]
+        # Go from [-pi, pi]
+        if x > np.pi:
+            x -= 2 * np.pi
+        # Convert to radians
+        x *= 23.9345 / (2 * np.pi)
+        y = np.load(y_path)[0]
+        if args.shape == 'total':
+            if args.g is not None:
+                occ = np.mean(occ_dict[obs][good_freqs])
+            else:
+                occ = np.mean(occ_dict[obs])
         else:
-            match_event_frac.append(0)
+            occ = occ_dict[obs]
+        scat_dat[i] = [x, y, occ]
     else:
-        match_event_frac.append(0)
+        missing_obs.append(obs)
 
-p_obs = [1061313616, 1061315448, 1061317272, 1061663760]
+util.make_obsfile(missing_obs, '%s/missing_obs.txt' % args.outdir)
+scat_dat = scat_dat[scat_dat[:, 1] > 0]
+print(scat_dat.shape)
 
-point_switch = []
-for obs in p_obs:
-    lst_p = np.load('/Users/mike_e_dubs/MWA/INS/Long_Run/All/metadata/%i_lst_array.npy' % (obs))[0]
-    if lst_p > np.pi:
-        lst_p -= 2 * np.pi
-    lst_p *= 23.9345 / (2 * np.pi)
-    point_switch.append(lst_p)
+counts, bins = np.histogram(scat_dat[:, 2], bins='auto')
+counts = np.append(counts, 0)
+where_0 = scat_dat[:, 2] == 0
+Nz = np.count_nonzero(where_0)
 
-fig, ax = plt.subplots(figsize=(14, 8))
-bool_0 = (np.array(match_event_frac) == 0).astype(bool)
-bool_1 = np.logical_not(bool_0)
-lst_0 = np.array(lst)[bool_0]
-JD_0 = np.array(JD)[bool_0]
-lst_1 = np.array(lst)[bool_1]
-JD_1 = np.array(JD)[bool_1]
-mef_1 = np.array(match_event_frac)[bool_1]
-pl.scatter_plot_2d(fig, ax, lst_0, JD_0, c='white', edgecolors='brown',
-                   vmin=1 / max_JD_len, vmax=0.4, xlabel='LST (hours)', ylabel='JD (days)',
-                   cbar_label='Occupancy Fraction')
-pl.scatter_plot_2d(fig, ax, lst_1, JD_1, c=mef_1, cmap=cm.copper_r,
-                   vmin=1 / max_JD_len, vmax=0.4, xlabel='LST (hours)', ylabel='JD (days)',
-                   cbar_label='Occupancy Fraction')
-for switch in point_switch:
-    ax.axvline(x=switch, color='black')
-fig.savefig(args.outpath)
+fig_scat, ax_scat = plt.subplots(figsize=(8, 4.5))
+fig_hist, ax_hist = plt.subplots(figsize=(8, 4.5))
+
+if args.vmax:
+    vmax = args.vmax
+else:
+    vmax = 0.4
+
+thresh_obs = [key for key in occ_dict.keys() if occ_dict[key] > vmax]
+util.make_obsfile(thresh_obs, '%s/thresh_obs_%.2f.txt' % (args.outdir, vmax))
+
+plot_lib.scatter_plot_2d(fig_scat, ax_scat, scat_dat[:, 0][where_0],
+                         scat_dat[:, 1][where_0], c='white',
+                         edgecolors='brown', s=20)
+
+plot_lib.scatter_plot_2d(fig_scat, ax_scat,
+                         scat_dat[:, 0][np.logical_not(where_0)],
+                         scat_dat[:, 1][np.logical_not(where_0)],
+                         c=scat_dat[:, 2][np.logical_not(where_0)],
+                         cmap=cm.copper_r,
+                         title='Occupation (%s) Scatter, %i$\sigma$' % (args.shape, args.sig_thresh),
+                         xlabel='LST (hours)', ylabel='Julian Date (Days)', s=20,
+                         vmax=vmax, cbar_label='Occupation Fraction')
+
+plot_lib.error_plot(fig_hist, ax_hist, bins, counts, drawstyle='steps-post',
+                    title='Occupation (%s) Histogram %i$\sigma$' % (args.shape, args.sig_thresh),
+                    xlabel='Occupation Fraction', ylabel='Counts',
+                    label='$N_z = %i$' % Nz, legend=True, leg_size='xx-large',
+                    ylim=[0.5, 2 * np.amax(counts)], yscale='log')
+
+np.save('%s/scat_dat_%i_%s.npy' % (args.outdir, args.sig_thresh, args.shape), scat_dat)
+np.save('%s/counts_%i_%s.npy' % (args.outdir, args.sig_thresh, args.shape), counts[:-1])
+np.save('%s/bins_%i_%s.npy' % (args.outdir, args.sig_thresh, args.shape), bins)
+
+fig_scat.savefig('%s/scat_%i_%s.png' % (args.outdir, args.sig_thresh, args.shape))
+fig_hist.savefig('%s/hist_%i_%s.png' % (args.outdir, args.sig_thresh, args.shape))
