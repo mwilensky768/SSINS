8ac5bd7318f28ed020023f76bdca991d44847695
Mike Wilensky <mjw768@uw.edu>
2019-01-18 14:22:19 -0800

    Did documentation for ES class

--- SSINS/event_stat.py
+++ SSINS/event_stat.py
@@ -1,5 +1,5 @@
 """
-Class for more deeply examining events which were identified in INS.
+Class for more deeply examining events that were identified in INS.
 """
 
 from __future__ import absolute_import, division, print_function
@@ -17,8 +17,7 @@ class ES:
     ES stands for Event Statistics. While the INS averages over baselines to
     locate RFI events in time-frequency, this class takes those events and
     averages across time-frequency for those baselines to see if particular
-    baselines are more contaminated than others. This is often used more like a
-    function than an interactive class.
+    baselines are more contaminated than others.
     """
 
     def __init__(self, data=None, flag_choice=None, events=None, MLE=None,
@@ -29,52 +28,39 @@ class ES:
         """
         init function for ES class. Attributes can be read in from numpy loadable
         files using the read_paths dictionary keyword, otherwise they can be set
-        manually.
+        manually. The new flags are generated upon instantiation. This process uses
+        Monte-Carlo to approximate the thermal distribution for sufficiently
+        narrow events and draws a cutoff between the baselines exhibiting
+        reasonable thermal noise and those exhibiting possible RFI
+        contamination. This is a very slow algorithm at present.
 
-        Required Parameters: data: The sky-subtracted visibility amplitudes to
-                                   be examined.
+        Args:
+            data: The sky-subtracted visibility amplitudes to be examined.
+                  Usually come from an SS object.
+            events (sequence): The events found by some of the filter tests in
+                               the MF class.
+            MLE: The maximum likelihood estimator for the Rayleigh width as
+                 calculated in the VDH class
+            uvw_array: The uvw_array as found in a UVData object
+            MC_iter (int): The number of simulated histograms to average
+                           together when flagging averaged events.
+            grid_dim (int): The number of pixels to have in each dimension of
+                            the uv-grid
+            grid_lim (float): The u and v bounds for the grid.
+            R_thresh (float): Describes the sensitivity of the flagger. Should
+                              be greater than 2.
+            read_paths (dict): A dictionary whose keys are the attributes to be
+                               read in, and whose values are paths to files
+                               containing the data for those attributes.
+            flag_choice (str): The flag choice used for the sky-subtracted data.
+                               (optional)
+            vis_units (str): THe units for the visibilities (optional)
+            obs (str): The OBSID for the observation in question (optional)
+            pols (sequence): The polarizations present in the data (optional)
+            outpath (str): A base directory to save results to (optional)
+            freq_array (sequence): The frequencies present in the data, in hz
+                                   (optional)
 
-                             events: The events found by some of the filter
-                                     tests in the MF class.
-
-                             MLE: The maximimum likelihood estimator for the
-                                  Rayleigh width as calculated in the VDH class.
-
-                             uvw_array: The uvw_array as found in a pyuvdata
-                                        object
-
-                             MC_iter: The number of simulated histograms to
-                                      average together when flagging averaged
-                                      events. Event just 100 iterations can be
-                                      extremely time-intensive.
-
-                             grid_dim: The number of pixels to have in each
-                                       dimension of the uv-grid.
-
-                             grid_lim: The u and v bounds for the uv-grid.
-
-                             R_thresh: Describes the sensitivity of the flagger.
-                                       Should be greater than 2.
-
-        Optional Parameters: flag_choice: The flag choice used for the
-                                          sky-subtracted data.
-
-                             vis_units: The units for the sky-subtracted
-                                        visibility amplitudes.
-
-                             obs: The OBSID for the observation in question.
-
-                             pols: The polarizations present in the data.
-
-                             outpath: A base directory to save results to.
-
-                             freq_array: The frequencies present in the data.
-
-        The new flags are generated upon instantiation. This process uses
-        Monte-Carlo to approximate the thermal distribution and draw
-        a cutoff between the baselines exhibiting reasonable thermal noise and
-        those exhibiting possible RFI contamination. This is a very slow
-        algorithm at present.
         """
 
         if not read_paths:
@@ -91,12 +77,16 @@ class ES:
             if grid_lim is None:
                 self.grid_lim = [self.uvw_array[:, :-1].min(),
                                  self.uvw_array[:, :-1].max()]
+                """The limits for u and v in the gridding step. Should be a pair
+                of floats."""
             else:
                 self.grid_lim = grid_lim
 
             self.grid = np.linspace(min(self.grid_lim), max(self.grid_lim),
                                     num=grid_dim + 1)
+            """The grid edges for the uv-grid."""
             self.Nbls = data.shape[1]
+            """The total number of baselines in the data."""
             temp_mask = np.zeros(data.shape, dtype=bool)
 
             attr_list = ['avgs', 'counts', 'exp_counts', 'exp_error', 'bins',
@@ -107,12 +97,12 @@ class ES:
             if events is not None and len(events):
                 print('Beginning bl_avg flagging at %s' % time.strftime("%H:%M:%S"))
                 for event in events:
-                    avg, counts, exp_counts, exp_error, bins = self.event_avg(data, event)
-                    lcut, rcut = self.cutoff(counts, bins, exp_counts, R_thresh)
+                    avg, counts, exp_counts, exp_error, bins = self._event_avg(data, event)
+                    lcut, rcut = self._cutoff(counts, bins, exp_counts, R_thresh)
                     cut_cond = np.logical_or(avg > rcut, avg < lcut)
                     cut_ind = np.where(cut_cond)
                     temp_mask[event[0], cut_ind[0], 0, event[2]] = 1
-                    uv_grid = self.bl_grid(avg, event)
+                    uv_grid = self._bl_grid(avg, event)
                     for attr, calc in zip(attr_list, (avg, counts, exp_counts,
                                                       exp_error, bins, uv_grid,
                                                       np.array([lcut, rcut]))):
@@ -120,15 +110,16 @@ class ES:
                 for attr in attr_list:
                     setattr(self, attr, np.array(getattr(self, attr)))
                 self.mask = temp_mask
+                """The final output mask obtained from the calculations."""
                 print('Done with bl_avg flagging at %s' % time.strftime("%H:%M:%S"))
             else:
                 print('No events given to ES class. Not computing flags.')
         else:
-            self.read(read_paths)
+            self._read(read_paths)
 
     def save(self):
         """
-        Writes out the attributes of this class.
+        Writes out the attributes of this class to ES.outpath.
         """
         for subdir in ['arrs', 'metadata']:
             path = '%s/%s' % (self.outpath, subdir)
@@ -149,10 +140,15 @@ class ES:
                       (self.outpath, self.obs, self.flag_choice, attr), 'wb') as f:
                 pickle.dump(getattr(self, attr), f)
 
-    def read(self, read_paths):
+    def _read(self, read_paths):
         """
         Reads in the attributes which are present in the read_paths dictionary.
-        See init docstring for details on which attributes are optional.
+
+        Args:
+            read_paths (dict): The keys of this dictionary should be the
+                               attributes to read in, while the values should be
+                               paths to files containing the information for
+                               those attributes.
         """
         for attr in ['vis_units', 'pols', 'grid', 'freq_array']:
             if attr not in read_paths or read_paths[attr] is None:
@@ -167,17 +163,26 @@ class ES:
                  read_paths entry'
             setattr(self, attr, np.load(read_paths[attr]))
 
-    def event_avg(self, data, event):
+    def _event_avg(self, data, event):
 
         """
         This takes an event (time-frequency) and averages the visibility
         difference amplitudes across that event for each baseline. Then,
-        simulated noise is averaged across that same event, where the noise
-        is estimated using the maximum-likelihood estimator (calculated in a VDH
-        instance) for the noise in each baseline/frequency/polarization. This
-        simulated averaged noise is histogrammed, and this process is repeated
-        MC_iter times. The histograms are averaged together to draw an
-        empirical thermal distribution.
+        an empirical thermal distribution is calculated using monte carlo,
+        except when events are broad enough that the central limit theorem
+        holds to good approximation (>30 frequency channels).
+
+        Args:
+            data: The sky-subtracted visibilities
+            event: An event over which to average
+
+        Returns:
+            avg: The averaged data
+            counts: The counts in each bin
+            exp_counts: The expected counts from the thermal estimation
+            exp_error: The expected variation of the counts in the bins (1
+                       standard deviation)
+            bins: The bin edges for the averaged amplitudes
         """
 
         avg = data[event[0], :, 0, event[2]]
@@ -197,12 +202,22 @@ class ES:
 
         return(avg, counts, exp_counts, exp_error, bins)
 
-    def cutoff(self, counts, bins, exp_counts, R_thresh):
+    def _cutoff(self, counts, bins, exp_counts, R_thresh):
 
         """
         This function takes the histogrammed, averaged data, and compares it to
         the empirical distribution drawn from event_avg(). Cutoffs are drawn
         based on R_thresh, which is a ratio of counts.
+
+        Args:
+            counts: The counts in each bin
+            bins: The bin edges for the averaged visibility difference amplitudes
+            exp_counts: The expected counts in each bin
+            R_thresh: The ratio of counts to exp_counts where the cutoff for
+            outliers should be drawn
+        Returns:
+            lcut: The cutoff bin edge on the left-hand-side of the distribution
+            rcut: The cutoff bin edge for the right-hand-side of the distribution
         """
 
         max_loc = bins[:-1][exp_counts.argmax()] + 0.5 * (bins[1] - bins[0])
@@ -220,12 +235,19 @@ class ES:
 
         return(lcut, rcut)
 
-    def bl_grid(self, avg, event):
+    def _bl_grid(self, avg, event):
 
         """
         This takes time-frequency averaged data from event_avg() and coarsely
         grids it in the UV-plane at the time of the event. Each pixel is
         averaged across the baselines whose centers lie within the pixel.
+
+        Args:
+            avg: The averaged visibility difference amplitudes
+            event: The event over which the average was performed
+        Returns:
+            uv_grid: A grid with average baseline power for baselines within each
+            pixel, over the subband corresponding to the event.
         """
 
         u = self.uvw_array[event[0] * self.Nbls:(event[0] + 1) * self.Nbls, 0]
