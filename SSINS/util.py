1828c16e4975a7b177cc20fe3e37d71695c6f147
Mike Wilensky <mjw768@uw.edu>
2019-02-06 13:22:49 -0800

    Added function to sort redundant events as well as a unit test for said function

--- SSINS/util.py
+++ SSINS/util.py
@@ -9,6 +9,7 @@ import scipy.stats
 import numpy as np
 import os
 from astropy.io import fits
+from functools import reduce
 
 
 def hist_fit(counts, bins, dist='norm'):
@@ -101,13 +102,39 @@ def slc_len(slc, shape):
     return(slc.indices(shape)[1] - slc.indices(shape)[0])
 
 
+def red_event_sort(match_events, shape_tuples, keep_prior=[1, 0]):
+    """
+    This is a function that is used to filter redundant events from a match_events
+    list, in case events with different names (or even shapes) are physically caused
+    by the same mechanism.
+    """
+    removal_list = []
+    # iterate through the shape pairs
+    for tup in shape_tuples:
+        # Separate the events belonging to each member of the shape_tuple
+        event_time_lists = [[event[0] for event in match_events if event[-1] == tup[i]] for i in range(len(tup))]
+        all_times, counts = np.unique(reduce((lambda x, y: x + y), event_time_lists), return_counts=True)
+        for event_time, count in zip(all_times, counts):
+            if count > 0:
+                list_ind = []
+                for i in range(len(tup)):
+                    if event_time in event_time_lists[i]:
+                        list_ind.append(i)
+                # Returns the list of indices for event_time_lists where the event must be removed
+                removal_list_inds = sorted(list_ind, key=lambda x: keep_prior.index(x))[1:]
+                for i in removal_list_inds:
+                    removal_list.append((event_time, tup[i]))
+    match_events = [event for event in match_events if (event[0], event[-1]) not in removal_list]
+
+    return(match_events)
+
+
 def event_fraction(match_events, Ntimes, shape_list, Nfreqs):
     """
     Calculates the fraction of times an event was caught by the flagger for
     each type of event.
     """
     shapes, counts = np.unique(np.array(match_events)[:, -1], return_counts=True)
-    # Explicit for loop since problem with dict comp involving unhashable types
     match_event_frac = {shape: 0 for shape in shape_list}
     for i, shape in enumerate(shapes):
         if shape is 'point':
