53c4b29144ea43eeb714cfb2d4061c2e0e0ca44e
Mike Wilensky <mjw768@uw.edu>
2019-02-20 08:01:07 -0800

    remove parentheses around assert statements

--- SSINS/tests/test_SS.py
+++ SSINS/tests/test_SS.py
@@ -26,14 +26,14 @@ def test_SS_read():
 
     # Test reading in only metadata skips if block
     ss.read(testfile, read_data=False)
-    assert(ss.data_array is None, "Data array is not None")
+    assert ss.data_array is None, "Data array is not None"
 
     # Test select on read and diff
     ss.read(testfile, times=np.unique(ss.time_array)[1:10])
-    assert(ss.Ntimes == 8, "Diff seems like it wasn't executed correctly")
+    assert ss.Ntimes == 8, "Diff seems like it wasn't executed correctly"
 
     # See that it still passes UVData check
-    assert(ss.check())
+    assert ss.check()
 
 
 def test_apply_flags():
@@ -47,31 +47,31 @@ def test_apply_flags():
     ss.read(testfile)
 
     # Make sure no flags are applied to start with
-    assert(not np.any(ss.data_array.mask), "There are some flags to start with.")
+    assert not np.any(ss.data_array.mask), "There are some flags to start with."
 
     # Apply flags, test equality, test attribute change
     ss.apply_flags(flag_choice='original')
-    assert(np.all(ss.flag_array == ss.data_array.mask), "Flag arrays are not equal")
-    assert(ss.flag_choice is 'original', "Flag choice attribute was not changed")
+    assert np.all(ss.flag_array == ss.data_array.mask), "Flag arrays are not equal"
+    assert ss.flag_choice is 'original', "Flag choice attribute was not changed"
 
     # Revert flags back, test equality, test attribute change
     ss.apply_flags(flag_choice=None)
-    assert(not np.any(ss.data_array.mask), "Flags did not revert back back")
-    assert(ss.flag_choice is None, "Flag choice attribute did not revert back")
+    assert not np.any(ss.data_array.mask), "Flags did not revert back back"
+    assert ss.flag_choice is None, "Flag choice attribute did not revert back"
 
     # Make a custom flag array where everything is flagged, check application
     custom = np.ones_like(ss.flag_array)
     ss.apply_flags(flag_choice='custom', custom=custom)
-    assert(np.all(ss.data_array.mask), "The custom flag array was not applied")
-    assert(ss.flag_choice is 'custom', "The flag choice attribute was not changed")
+    assert np.all(ss.data_array.mask), "The custom flag array was not applied"
+    assert ss.flag_choice is 'custom', "The flag choice attribute was not changed"
 
     # Read an INS in (no flags by default) and flag a channel, test if it applies correctly
     ins = INS(insfile)
     ins.data_array.mask[:, 0] = True
     ss.apply_flags(flag_choice='INS', INS=ins)
-    assert(np.all(ss.data_array.mask[:, 0, 0]), "Not all of the 0th channel was flagged.")
-    assert(not np.any(ss.data_array.mask[:, 0, 1:]), "Some of the channels other than the 0th were flagged")
-    assert(ss.flag_choice is 'INS')
+    assert np.all(ss.data_array.mask[:, 0, 0]), "Not all of the 0th channel was flagged."
+    assert not np.any(ss.data_array.mask[:, 0, 1:]), "Some of the channels other than the 0th were flagged"
+    assert ss.flag_choice is 'INS'
 
 
 def test_INS_prepare():
@@ -93,9 +93,9 @@ def test_INS_prepare():
     test_weights = np.sum(np.logical_not(ss.data_array.mask).reshape(new_shape), axis=1)
 
     # Check that the data array averaged correctly
-    assert(np.all(test_dat == ss.INS.metric_array), "Averaging did not work as intended.")
+    assert np.all(test_dat == ss.INS.metric_array), "Averaging did not work as intended."
     # Check that the weights summed correctly
-    assert(np.all(test_weights == ss.INS.weights_array), "Weights did not sum properly")
+    assert np.all(test_weights == ss.INS.weights_array), "Weights did not sum properly"
 
 
 def test_mixture_prob():
@@ -115,7 +115,7 @@ def test_mixture_prob():
     mixture_prob = ss.mixture_prob(bins=bins)
 
     # Check that they sum to 1
-    assert(np.sum(mixture_prob) == 1, "Probabilities did not add up to 1")
+    assert np.sum(mixture_prob) == 1, "Probabilities did not add up to 1"
 
 
 def test_rev_ind():
@@ -142,7 +142,7 @@ def test_rev_ind():
     wf_hist = ss.rev_ind(band)
 
     # Check that it picked up that point
-    assert(wf_hist[t, f, p] == 1, "The algorithm did not find the data point")
+    assert wf_hist[t, f, p] == 1, "The algorithm did not find the data point"
 
     # Check no other points were picked up
-    assert(np.count_nonzero(wf_hist) == 1, "The algorithm found other data")
+    assert np.count_nonzero(wf_hist) == 1, "The algorithm found other data"
