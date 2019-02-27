073e85a0678d5f6260fccbda6daaf44202ffb968
Mike Wilensky <mjw768@uw.edu>
2019-02-27 15:52:53 -0800

    ignore annoying cython warnings

--- SSINS/__init__.py
+++ SSINS/__init__.py
@@ -4,6 +4,12 @@ from __future__ import absolute_import, division, print_function
 init file for SSINS
 """
 
+# Filter annoying Cython warnings that serve no good purpose. see numpy#432
+# needs to be done before the imports to work properly
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 from .event_stat import *
 from .incoherent_noise_spectrum import *
 from .plot_lib import *
