2c616c7260d8582d0535d422db21cb05092c74a3
Mike Wilensky <mjw768@uw.edu>
2019-02-28 13:24:29 -0800

    Added samp_thresh tests/refactor

--- SSINS/tests/test_MF.py
+++ SSINS/tests/test_MF.py
@@ -7,22 +7,6 @@ import numpy as np
 import pytest
 
 
-def INS_mock():
-
-    obs = '1061313128_99bl_1pol_half_time'
-    insfile = os.path.join(DATA_PATH, '%s_SSINS.uvh5' % obs)
-
-    ins = INS(insfile)
-
-    # Mock a simple metric_array and freq_array
-    ins.metric_array = np.ma.ones([10, 20, 1])
-    ins.weights_array = np.copy(ins.metric_array)
-    ins.freq_array = np.zeros([1, 20])
-    ins.freq_array[0, :] = np.arange(20)
-
-    return(ins)
-
-
 def test_init():
 
     freq_path = os.path.join(DATA_PATH, 'MWA_Highband_Freq_Array.npy')
@@ -36,33 +20,42 @@ def test_init():
     mf_1 = MF(freqs, 5, shape_dict=shape_dict)
 
     assert mf_1.slice_dict['shape'] == slice(0, 5), "It did not set the shape correctly"
-    assert mf_1.slice_dict['point'] is None, "Point did not get set correctly"
-    assert mf_1.slice_dict['streak'] == slice(None), "Streak did not get set correctly"
+    assert mf_1.slice_dict['narrow'] is None, "narrow did not get set correctly"
+    assert mf_1.slice_dict['streak'] == slice(None), "streak did not get set correctly"
 
-    # Test disabling streak/point
-    mf_2 = MF(freqs, 5, shape_dict=shape_dict, point=False, streak=False)
+    # Test disabling streak/narrow
+    mf_2 = MF(freqs, 5, shape_dict=shape_dict, narrow=False, streak=False)
 
-    assert 'point' not in mf_2.slice_dict, "Point is still in the shape_dict"
-    assert 'streak' not in mf_2.slice_dict, "Streak still in shape_dict"
-    assert 'shape' in mf_2.slice_dict, "Shape not in shape_dict"
+    assert 'narrow' not in mf_2.slice_dict, "narrow is still in the shape_dict"
+    assert 'streak' not in mf_2.slice_dict, "streak still in shape_dict"
+    assert 'shape' in mf_2.slice_dict, "shape not in shape_dict"
 
     # Test if error gets raised with bad shape_dict
     try:
-        mf_3 = MF(freqs, 5, shape_dict={}, streak=False, point=False)
+        mf_3 = MF(freqs, 5, shape_dict={}, streak=False, narrow=False)
     except ValueError:
         pass
 
 
 def test_match_test():
 
-    ins = INS_mock()
+    obs = '1061313128_99bl_1pol_half_time'
+    insfile = os.path.join(DATA_PATH, '%s_SSINS.uvh5' % obs)
+
+    ins = INS(insfile)
+
+    # Mock a simple metric_array and freq_array
+    ins.metric_array = np.ma.ones([10, 20, 1])
+    ins.weights_array = np.copy(ins.metric_array)
+    ins.freq_array = np.zeros([1, 20])
+    ins.freq_array[0, :] = np.arange(20)
 
     # Make a shape dictionary for a shape that will be injected later
     shape = [7.9, 12.1]
     shape_dict = {'shape': shape}
     mf = MF(ins.freq_array, 5, shape_dict=shape_dict)
 
-    # Inject a shape, point, and streak event
+    # Inject a shape, narrow, and streak event
     ins.metric_array[3, 5] = 10
     ins.metric_array[5] = 10
     ins.metric_array[7, 7:13] = 10
@@ -78,14 +71,23 @@ def test_match_test():
 
 def test_apply_match_test():
 
-    ins = INS_mock()
+    obs = '1061313128_99bl_1pol_half_time'
+    insfile = os.path.join(DATA_PATH, '%s_SSINS.uvh5' % obs)
+
+    ins = INS(insfile)
+
+    # Mock a simple metric_array and freq_array
+    ins.metric_array = np.ma.ones([10, 20, 1])
+    ins.weights_array = np.copy(ins.metric_array)
+    ins.freq_array = np.zeros([1, 20])
+    ins.freq_array[0, :] = np.arange(20)
 
     # Make a shape dictionary for a shape that will be injected later
     shape = [7.9, 12.1]
     shape_dict = {'shape': shape}
     mf = MF(ins.freq_array, 5, shape_dict=shape_dict)
 
-    # Inject a shape, point, and streak event
+    # Inject a shape, narrow, and streak event
     ins.metric_array[3, 5] = 10
     ins.metric_array[5] = 10
     ins.metric_array[7, 7:13] = 10
@@ -103,4 +105,36 @@ def test_apply_match_test():
 
     assert es.match_events == [(5, slice(None), 'streak'),
                                (7, slice(7, 13), 'shape'),
-                               (3, slice(5, 6), 'point')], "Events are incorrect"
+                               (3, slice(5, 6), 'narrow')], "Events are incorrect"
+
+
+def test_samp_thresh():
+
+    obs = '1061313128_99bl_1pol_half_time'
+    insfile = os.path.join(DATA_PATH, '%s_SSINS.uvh5' % obs)
+
+    ins = INS(insfile)
+
+    # Mock a simple metric_array and freq_array
+    ins.metric_array = np.ma.ones([10, 20, 1])
+    ins.weights_array = np.copy(ins.metric_array)
+    ins.freq_array = np.zeros([1, 20])
+    ins.freq_array[0, :] = np.arange(20)
+
+    # Arbitrarily flag enough data in channel 10
+    mf = MF(ins.freq_array, 5, streak=False, N_samp_thresh=5)
+    ins.metric_array[3:, 10] = np.ma.masked
+    ins.metric_array[3:, 9] = np.ma.masked
+    # Put in an outlier so it gets to samp_thresh_test
+    ins.metric_array[0, 11] = 10
+    ins.metric_ms = ins.mean_subtract()
+    bool_ind = np.zeros(ins.metric_array.shape, dtype=bool)
+    bool_ind[:, 10] = 1
+    bool_ind[:, 9] = 1
+    bool_ind[0, 11] = 1
+
+    es = mf.apply_match_test(ins, event_record=True, apply_samp_thresh=True)
+
+    # Test stuff
+    assert np.all(ins.metric_array.mask == bool_ind), "The right flags were not applied"
+    assert np.all(es.samp_thresh_events == np.array([9, 10])), "The events weren't appended correctly"
