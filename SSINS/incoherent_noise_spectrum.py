9c07a9faecb99be116ed531c8404ae010e873ab8
Mike Wilensky <mjw768@uw.edu>
2019-05-07 11:09:03 -0700

    reformat INS strings again

--- SSINS/incoherent_noise_spectrum.py
+++ SSINS/incoherent_noise_spectrum.py
@@ -140,9 +140,13 @@ class INS(UVFlag):
             data_compression: See UVFlag documentation
             output_type ('data', 'z_score', 'mask', 'flags', 'match_events'):
                 data: outputs the metric_array attribute into an h5 file
+
                 z_score: outputs the the metric_ms attribute into an h5 file
+
                 mask: outputs the mask for the metric_array attribute into an h5 file
+
                 flags: converts mask to flag using mask_to_flag() method and writes to an h5 file readable by UVFlag
+
                 match_events: Writes the match_events attribute out to a human-readable yml file
         """
 
