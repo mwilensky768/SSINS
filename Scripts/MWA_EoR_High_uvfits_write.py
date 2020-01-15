92c1e60a7d1baf443a5afb2d2d44aff786501104
Mike Wilensky <mjw768@uw.edu>
2020-01-15 12:20:23 -0800

    Adjust reading for mwa_corr_fits functionality

--- Scripts/MWA_EoR_High_uvfits_write.py
+++ Scripts/MWA_EoR_High_uvfits_write.py
@@ -10,6 +10,7 @@ parser.add_argument('-i', '--insfile', help='The path to the input file')
 parser.add_argument('-m', '--maskfile', help='The path to the masks')
 parser.add_argument('-d', '--outdir', help='The output directory')
 parser.add_argument('-u', '--uvd', nargs='*', help='The path to the uvdata files')
+parser.add_argument('-t', '--uvd_type', help="The type of visibility file")
 parser.add_argument('-n', '--nsample_default', default=1, type=float, help='The default nsample to use.')
 parser.add_argument('-f', '--rfi_flag', type='store_true', help="Whether or not to do rfi flagging with SSINS")
 args = parser.parse_args()
@@ -26,6 +27,13 @@ if args.rfi_flag:
         ins = INS(args.insfile, mask_file=args.maskfile)
     else:
         ss = SS()
+        if args.uvd_type is 'uvfits':
+            ss.read(args.uvd)
+        elif args.uvd_type is 'gpubox':
+            ss.read_mwa_corr_fits(args.uvd)
+        else:
+            raise ValueError("uvd_type option has an invalid value. Choose either 'uvfits' or 'gpubox'")
+        ss.select(times=np.unique(ss.time_array)[3:-3])
         ins = INS(ss)
         freqs = np.arange(1.7e8, 2e8, 5e6)
         xticks, xticklabels = util.make_ticks_labels(freqs, ins.freq_array, sig_fig=0)
@@ -40,6 +48,7 @@ if args.rfi_flag:
                               xlabel='Frequency (Mhz)', ylabel='Time (UTC)')
         # Try to save memory - hope for garbage collector
         del ss
+        # Set up MF flagging for routine shapes
         shape_dict = {'TV6': [1.74e8, 1.81e8],
                       'TV7': [1.81e8, 1.88e8],
                       'TV8': [1.88e8, 1.95e8],
@@ -58,16 +67,17 @@ if args.rfi_flag:
                               xlabel='Frequency (Mhz)', ylabel='Time (UTC)')
 
     uvd = UVData()
-    if len(args.uvd) > 1:
+    if args.uvd_type is 'gpubox':
         uvd.read_mwa_corr_fits(args.uvd)
-    else:
+    elif args.uvd_type is 'uvfits':
         uvd.read(args.uvd)
+    else:
+        raise ValueError("uvd_type option has an invalid value. Choose either 'uvfits' or 'gpubox'")
     uvd.select(times=np.unique(uv.time_array)[3:-3])
     uvf = ins.copy()
     uvf.to_flag()
     uvf.flag_array = ins.mask_to_flags()
     utils.apply_uvflag(uvd, uvf, inplace=False)
-
 
 if np.any(uvd.nsample_array == 0):
     uvd.nsample_array[uvd.nsample_array == 0] = args.nsample_default
