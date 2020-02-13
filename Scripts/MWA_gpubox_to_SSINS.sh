d556fc46341634819534e76564dc61971c130ad3
Mike Wilensky <mjw768@uw.edu>
2020-02-14 05:44:47 +0800

    Adjust for singularity call

--- Scripts/MWA_gpubox_to_SSINS.sh
+++ Scripts/MWA_gpubox_to_SSINS.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-module use /group/mwa/software/modulefiles
-module load six
-module load pyuvdata
-module load h5py
-module load scipy
-module load matplotlib
-module load numpy
-module load pyyaml
+#SBATCH --nodes=1
+#SBATCH --ntasks=18
+#SBATCH --time=01:00:00
+#SBATCH --account=mwaeor
+#SBATCH	--output=SSINS_%j.out
+#SBATCH --error=SSINS_%j.err
+
+module load singularity
+export SINGULARITY_CACHEDIR=$MYGROUP/singularity_cache
+
+#module use /group/mwa/software/modulefiles
+#module load six
+#module load pyuvdata
+#module load h5py
+#module load scipy
+#module load matplotlib
+#module load numpy
+#module load pyyaml
 
 data_dir=/astro/mwaeor/MWA/data
 
@@ -22,5 +32,8 @@ if [ ! -e ${outdir}/${obs}_SSINS_data.h5 ]; then
 
   echo "Executing python script for ${obs}"
   gpufiles=$(ls ${data_dir}/${obs}/*gpubox*.fits)
-  python MWA_vis_to_SSINS.py -f $gpufiles -o $obs -d ${outdir}
+  metafile=$(ls ${data_dir}/${obs}/*.metafits)
+  srun --export=ALL singularity exec zeus-python_latest.sif MWA_vis_to_SSINS.py -f $gpufiles $metafile -o $obs -d ${outdir}
+else
+  echo "Output already exists. Skipping this obsid ${obs}."
 fi
