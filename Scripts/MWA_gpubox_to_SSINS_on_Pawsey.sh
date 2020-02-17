434821d3a522f4455e7877a582af63d438ca051d
Mike Wilensky <mjw768@uw.edu>
2020-02-18 05:14:54 +0800

    Pawsey updates

--- Scripts/MWA_gpubox_to_SSINS_on_Pawsey.sh
+++ Scripts/MWA_gpubox_to_SSINS_on_Pawsey.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=18
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=14
 #SBATCH --time=01:00:00
 #SBATCH --account=mwaeor
 #SBATCH	--output=SSINS_%j.out
@@ -33,7 +34,11 @@ if [ ! -e ${outdir}/${obs}_SSINS_data.h5 ]; then
   echo "Executing python script for ${obs}"
   gpufiles=$(ls ${data_dir}/${obs}/*gpubox*.fits)
   metafile=$(ls ${data_dir}/${obs}/*.metafits)
-  srun --export=all -n 1 --cpus-per-task 28 env - $(which singularity) exec $SINGULARITY_CACHEDIR/python_ssins-2020-02-13.sif /bin/bash -c "MWA_vis_to_SSINS.py -f $gpufiles $metafile -o $obs -d ${outdir}"
+  filelist=""
+  for file in $gpufiles; do filelist="$filelist $file"; done
+  command="python3 /group/mwa/software/SSINS/csv_script/Scripts/MWA_vis_to_SSINS.py -o $obs -d ${outdir} -f $filelist $metafile"
+  srun --export=all env - $(which singularity) exec -B /astro -B /group -B /group/mwaeor/mwilensky $SINGULARITY_CACHEDIR/python_ssins-2020-02-13.sif /bin/bash -c "$command"
+#  srun --export=all -n 1 --cpus-per-task 1 env - $(which singularity) exec $SINGULARITY_CACHEDIR/python_ssins-2020-02-13.sif /bin/bash -c "python3 /group/mwa/software/SSINS/csv_script/Scripts/MWA_vis_to_SSINS.py -f $gpufiles $metafile -o $obs -d ${outdir}"
 else
   echo "Output already exists. Skipping this obsid ${obs}."
 fi
