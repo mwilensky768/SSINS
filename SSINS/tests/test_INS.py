bb221949361fa2eb85b7cf4dd276dd5dbd57531e
Mike Wilensky <mjw768@uw.edu>
2020-04-09 21:33:36 -0700

    make test much more straight-forward

--- SSINS/tests/test_INS.py
+++ SSINS/tests/test_INS.py
@@ -232,23 +232,14 @@ def test_write_mwaf():
     ins.metric_array = np.ma.ones([55, 384, 1])
     ins.metric_array[50, 16 * 12: int(16 * (12 + 0.5))] = np.ma.masked
 
-    # metadata from the input file, hardcoded for testing purposes
-    time_div = 4
-    freq_div = 2
+    # metadata from the input file
     NCHANS = 32
-    # boxes go backwards for this file
-    boxint = 24 - 12
     Nbls = 8256
     NSCANS = 224
-    flags = ins.mask_to_flags()
-    # Repeat in time
-    time_rep_flags = np.repeat(flags, time_div, axis=0)
-    # Repeat in freq
-    freq_time_rep_flags = np.repeat(time_rep_flags, freq_div, axis=1)
-    # Repeat in bls
-    freq_time_bls_rep_flags = np.repeat(freq_time_rep_flags[:, np.newaxis, NCHANS * boxint: NCHANS * (boxint + 1)], Nbls, axis=1)
-    # This shape is on MWA wiki. Reshape to this shape.
-    new_flags = freq_time_bls_rep_flags.reshape((NSCANS * Nbls, NCHANS))
+
+    # hard code the answer
+    new_flags = np.zeros((NSCANS * Nbls, NCHANS), dtype=bool)
+    new_flags[Nbls * 200:Nbls * 208, :16] = 1
 
     # Test some defensive errors
     with pytest.raises(IOError):
@@ -273,12 +264,6 @@ def test_write_mwaf():
             assert np.all(add_mwaf_hdu[1].data['FLAGS'] == old_mwaf_hdu[1].data['FLAGS'] + new_flags)
     with fits.open('%s_replace_12.mwaf' % prefix) as replace_mwaf_hdu:
         assert np.all(replace_mwaf_hdu[1].data['FLAGS'] == new_flags)
-    with fits.open('%s_replace_12.mwaf' % prefix) as replace_mwaf_hdu:
-        # Only the first half of these 8 integrations in this coarse channel should be flagged
-        assert np.all(replace_mwaf_hdu[1].data['FLAGS'][200 * Nbls:208 * Nbls, :16])
-        assert not np.any(replace_mwaf_hdu[1].data['FLAGS'][:200 * Nbls])
-        assert not np.any(replace_mwaf_hdu[1].data['FLAGS'][208 * Nbls:])
-        assert not np.any(replace_mwaf_hdu[1].data['FLAGS'][:, 16:])
 
     for path in ['%s_add_12.mwaf' % prefix, '%s_replace_12.mwaf' % prefix]:
         os.remove(path)
