b459f7eda16362ed403cdc58d49a921e1d17f1d4
Mike Wilensky <mjw768@uw.edu>
2020-07-07 15:47:41 -0700

    A can of worms

--- SSINS/match_filter.py
+++ SSINS/match_filter.py
@@ -3,6 +3,7 @@ Match Filter class
 """
 
 import numpy as np
+import warnings
 
 
 class MF():
@@ -12,7 +13,8 @@ class MF():
     """
 
     def __init__(self, freq_array, sig_thresh, shape_dict={}, N_samp_thresh=0,
-                 broadcast_dict=None, narrow=True, streak=True):
+                 narrow=True, streak=True, broadcast_dict={},
+                 broadcast_streak=False):
 
         """
         Args:
@@ -31,6 +33,8 @@ class MF():
                 when broadcasting over frequencies. The keys should be the names
                 of each subband to broadcast over, and the values should be the
                 edges of each subband (i.e. two-element lists).
+            broadcast_streak (bool): Optional, broadcast flags over whole
+                observing band.
             narrow (bool): If True, search for narrowband (single channel) RFI
             streak (bool): If True, search for broad RFI streaks that occupy the entire observing band
         """
@@ -46,7 +50,7 @@ class MF():
         self.N_samp_thresh = N_samp_thresh
         """The threshold for flagging an entire channel when some flags exist and apply_samp_thresh() is called.
            See apply_samp_thresh() documentation for exact meaning."""
-        self.slice_dict = self._shape_slicer(narrow, streak, shape_dict)
+        self.slice_dict = self._shape_slicer(narrow, streak, "shape_dict")
         """A dictionary whose keys are the same as shape_dict and whose values are corresponding slices into the freq_array attribute"""
         if type(self.sig_thresh) is dict:
             for shape in self.slice_dict:
@@ -59,9 +63,11 @@ class MF():
             self.sig_thresh = sig_thresh_dict
         self.broadcast_dict = broadcast_dict
         """A dictionary of subbands. Keys are a subband name, values are the lower and upper frequency bounds in Hz."""
-        if self.broadcast_dict is not None:
-            self.broadcast_slc_dict = self._shape_slicer(False, False, input_dict="broadcast_dict")
-            """A dictionary whose keys are the same as broadcast_dict and whose values are corresponding slices into the freq_array attribute"""
+
+        self.broadcast_slc_dict = self._shape_slicer(False, broadcast_streak,
+                                                     input_dict="broadcast_dict",
+                                                     check_overlap=True)
+        """A dictionary whose keys are the same as broadcast_dict and whose values are corresponding slices into the freq_array attribute"""
 
     def _shape_slicer(self, narrow, streak, input_dict="shape_dict"):
 
@@ -81,14 +87,14 @@ class MF():
 
         slice_dict = {}
         for shape in getattr(self, input_dict):
-            if min(self.freq_array) < min(getattr(self, input_dict)[shape]) or \
-               max(self.freq_array) > max(getattr(self, input_dict)[shape]):
+            if min(self.freq_array) <= min(getattr(self, input_dict)[shape]) or \
+               max(self.freq_array) >= max(getattr(self, input_dict)[shape]):
                 min_chan = np.argmin(np.abs(self.freq_array - min(getattr(self, input_dict)[shape])))
                 max_chan = np.argmin(np.abs(self.freq_array - max(getattr(self, input_dict)[shape])))
                 # May have to extend the edges depending on if the shape extends beyond the min and max chan infinitesimally
                 if (self.freq_array[min_chan] - min(getattr(self, input_dict)[shape]) > 0) and (min_chan > 0):
                     min_chan -= 1
-                if self.freq_array[max_chan] - max(getattr(self, input_dict)[shape]) < 0:
+                if self.freq_array[max_chan] - max(getattr(self, input_dict)[shape]) <= 0:
                     max_chan += 1
                 slice_dict[shape] = slice(min_chan, max_chan)
         if narrow:
@@ -125,6 +131,7 @@ class MF():
                 t, f, p = np.unravel_index(np.absolute(INS.metric_ms).argmax(),
                                            INS.metric_ms.shape)
                 sig = np.absolute(INS.metric_ms[t, f, p])
+                t = slice(t, t + 1)
                 f = slice(f, f + 1)
             else:
                 N = np.count_nonzero(np.logical_not(INS.metric_ms[:, self.slice_dict[shape]].mask),
@@ -132,6 +139,7 @@ class MF():
                 sliced_arr = np.absolute(INS.metric_ms[:, self.slice_dict[shape]].mean(axis=1)) * np.sqrt(N)
                 t, p = np.unravel_index((sliced_arr / self.sig_thresh[shape]).argmax(),
                                         sliced_arr.shape)
+                t = slice(t, t + 1)
                 f = self.slice_dict[shape]
                 sig = sliced_arr[t, p]
             if sig > self.sig_thresh[shape]:
@@ -169,10 +177,10 @@ class MF():
                 INS.sig_array[event[:2]][nonmask] = INS.metric_ms[event[:2]][nonmask]
                 if event_record:
                     INS.match_events.append(event)
-                if freq_broadcast:
-                    self.freq_broadcast(INS, event_record=event_record)
                 if (apply_samp_thresh and self.N_samp_thresh):
-                    self.apply_samp_thresh_test(INS, event_record=event_record)
+                    event = self.apply_samp_thresh_test(INS, event, event_record=event_record)
+                if freq_broadcast:
+                    self.freq_broadcast(INS, event, event_record=event_record)
                 if not np.all(INS.metric_array[:, f_max, 0].mask):
                     INS.metric_ms[:, f_max] = INS.mean_subtract(freq_slice=f_max)
                 else:
@@ -180,7 +188,7 @@ class MF():
         nonmask_all = np.logical_not(INS.metric_ms.mask)
         INS.sig_array[nonmask_all] = INS.metric_ms[nonmask_all]
 
-    def apply_samp_thresh_test(self, INS, event_record=False):
+    def apply_samp_thresh_test(self, INS, event, event_record=False):
         """
         The sample threshold test. This tests to see if any channels have fewer
         unflagged channels than the N_samp_thresh parameter. If so, the entire channel is
@@ -190,46 +198,52 @@ class MF():
 
         Args:
             INS: An INS to test
-            event_record (bool): If true, append events to INS.match_events
+            event: The event to check.
+            event_record (bool): If true, append events to INS.match_events.
+        Returns:
+            new_event: Possible new event if more flagging happened. Returns old
+                event if no additional flagging happened.
         """
 
-        if self.N_samp_thresh > INS.metric_array.shape[0]:
+        if self.N_samp_thresh > INS.Ntimes:
             raise ValueError("N_samp_thresh parameter is set higher than "
                              "the number of time samples. This will "
                              "always result in flagging the entire "
                              "observation. Aborting flagging.")
-        good_chans = np.where(np.logical_not(np.all(INS.metric_array[:, :, 0].mask, axis=0)))[0]
-        N_unflagged = INS.metric_array.shape[0] - np.count_nonzero(INS.metric_array.mask[:, good_chans, 0], axis=0)
-        if np.any(N_unflagged < self.N_samp_thresh):
-            good_chan_ind = np.where(N_unflagged < self.N_samp_thresh)[0]
+        num_chan = event[1].stop - event[1].start
+        num_flag = np.count_nonzero(INS.metric_array[:, event[1], 0].mask)
+        num_unflagged = INS.Ntimes - num_flag
+        if num_unflagged / num_chan < self.N_samp_thresh:
+            INS.metric_array[:, event[1]] = np.ma.masked
             if event_record:
-                for chan in good_chans[good_chan_ind]:
-                    event_times = np.nonzero(np.logical_not(INS.metric_array.mask[:, chan]))[0]
-                    INS.sig_array[event_times, chan] = INS.metric_ms[event_times, chan]
-                    for event_time in event_times:
-                        event = (event_time, slice(chan, chan + 1), 'samp_thresh',
-                                 None)
-                        INS.match_events.append(event)
-            INS.metric_array[:, good_chans[good_chan_ind]] = np.ma.masked
+                new_event = (slice(0, INS.Ntimes), event[1],
+                             f"samp_thresh_{event[2]}", None)
+                INS.match_events.append(new_event)
+        else:
+            new_event = event
 
-    def freq_broadcast(self, INS, event_record=False):
+        return(new_event)
+
+    def freq_broadcast(self, INS, event, event_record=False):
         """
         Broadcast flags in frequency. An event will be recorded in the
         match_filter saying which integration/band was flagged.
+
+        Args:
+            INS: The incoherent noise spectrum being worked on.
+            event: The event to broadcast flags for.
+            event_record (bool): Whether to record a new event for this flagging entry.
         """
         if self.broadcast_dict is None:
             raise ValueError("MF object does not have a broadcast_dict, but is "
                              " being asked to broadcast flags. Check "
                              " initialization of MF object.")
 
-        for sb_name in self.broadcast_slc_dict:
-            # Find times that are not all flagged but have some flags in this slice
-            time_inds = np.logical_xor(np.any(ins.metric_array.mask[:, self.broadcast_slc_dict[sb_name]]),
-                                       np.all(ins.metric_array.mask[:, self.broadcast_slc_dict[sb_name]]),
-                                       axis=[1, -1])
-            if np.any(time_inds):
-                ins.metric_array[time_inds, self.broadcast_slc_dict[sb_name]] = np.ma.masked
+        for sb in self.broadcast_slc_dict:
+            sb_slc = self.broadcast_slc_dict[sb]
+            event_set = set(np.arange(event[1].stop)[event[1]])
+            broad_set = set(np.arange(sb_slc.stop)[sb_slc])
+            if not event_set.isdisjoint(broad_set):
+                INS.metric_array[event[0], sb_slc] = np.ma.masked
                 if event_record:
-                    for event_time in np.arange(INS.Ntimes)[time_inds]:
-                        event = (event_time, self.broadvast_slc_dict[sb_name],
-                                 f'freq_broadcast_{sb_name}', None)
+                    INS.match_events.append((event[0], sb_slc, f"freq_broadcast_{sb}"))
