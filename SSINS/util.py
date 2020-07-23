f95f2796142736e10d8ce9c891634bdba4970090
Mike Wilensky <mjw768@uw.edu>
2020-07-22 17:43:07 -0700

    First pass at occ_calc function

--- SSINS/util.py
+++ SSINS/util.py
@@ -4,52 +4,11 @@ nice to have around.
 """
 import numpy as np
 import os
+from SSINS.match_filter import Event
 
 
-def red_event_sort(match_events, shape_tuples):
-
-    """
-    This is a function that is used to filter redundant events from a match_events
-    list, in case events with different names (or even shapes) are physically caused
-    by the same mechanism.
-
-    Args:
-        match_events: A list of match_events from an INS found by a match filter
-        shape_tuples:
-            A list of tuples where each tuple indicates which events are
-            considered redundant with one another. Each tuple must be ordered
-            according to the priority in which the shapes are to be kept.
-
-    Returns:
-        match_events: The filtered match_event list.
-    """
-
-    removal_events = []
-    # Iterate over the redundancy sets
-    for shape_tup in shape_tuples:
-        # Filter the shapes that are in the current shape tuple
-        relevant_events = [event for event in match_events if event[-2] in shape_tup]
-        all_times = [event[0] for event in relevant_events]
-        # Generate an array of unique times and counts
-        unique_times, counts = np.unique(all_times, return_counts=True)
-        # find times with redundancies
-        red_times = unique_times[counts > 1]
-        # iterate over the times with redundancies
-        for time in unique_times:
-            # Collect relevant events that occurred at each time
-            red_events = [event for event in relevant_events if event[0] == time]
-            # Sort them according to the priority in the shape tuple
-            sorted_red_events = sorted(red_events, key=lambda x: shape_tup.index(x[-2]))
-            # Add the tail of the list to the removal_list
-            removal_events += sorted_red_events[1:]
-
-    for event in removal_events:
-        match_events.remove(event)
-
-    return(match_events)
-
-
-def calc_occ(ins, mf, num_init_flag, num_int_flag=0, num_chan_flag=0):
+def calc_occ(ins, mf, num_init_flag, num_int_flag=0, num_chan_flag=0,
+             lump_narrowband=True):
     """
     Calculates the fraction of times an event was caught by the flagger for
     each type of event. Does not take care of frequency broadcasted events.
@@ -60,18 +19,13 @@ def calc_occ(ins, mf, num_init_flag, num_int_flag=0, num_chan_flag=0):
         num_init_flag: The number of initially flagged samples
         num_int_flag: The number of fully flagged integrations in the starting flags
         num_chan_flag: The number of channels fully flagged before the match filter.
+        lump_narrowband: Whether to combine narrowband occupancies into a single number.
+            Will slightly overestimate if n_int_flag > 0.
 
     Returns:
         occ_dict: A dictionary with shapes for keys and occupancy fractions for values
     """
     occ_dict = {shape: 0. for shape in mf.slice_dict}
-
-    for shape in occ_dict:
-        relevant_events = [event for event in ins.match_events if shape in event.shape]
-        num_time_event = sum([(event.time_slice.stop - event.time_slice.start) for event in relevant_events])
-        occ_dict[shape] = num_time_event / (ins.Ntimes - num_int_flag)
-        if shape == "narrow":
-            occ_dict[shape] /= (ins.Nfreqs - num_chan_flag)
 
     # Figure out the total occupancy sans initial flags
     total_data = np.prod(ins.metric_array.shape)
@@ -79,6 +33,56 @@ def calc_occ(ins, mf, num_init_flag, num_int_flag=0, num_chan_flag=0):
     total_flag = np.sum(ins.metric_array.mask)
     total_RFI = total_flag - num_init_flag
     total_occ = total_RFI / total_valid
+
+    for shape in occ_dict:
+        relevant_events = [event for event in ins.match_events if shape in event.shape]
+        shape_set = set()
+        time_range = np.arange(ins.Ntimes)
+        if shape == "narrow":
+            if lump_narrowband:
+                freq_range = np.arange(ins.Nfreqs)
+                for event in relevant_events:
+                    ntimes_event = event.time_slice.stop - event.time_slice.start
+                    event_set = set(zip(time_range[event.time_slice],
+                                        np.repeat(freq_range[event.freq_slice], ntimes_event)))
+                    shape_set = shape_set.union(event_set)
+                # The length of the shape_set tells you how many points were flagged, without overcounting
+                occ_dict[shape] = len(shape_set) / total_valid
+            else:
+                # Need to relabel them by frequency
+                relabeled_events = []
+                for event_ind, event in enumerate(relevant_events):
+                    event_freq = (ins.freq_array[event.freq_slice] * 10**(-6))[0]
+                    relabeled_event = Event(event.time_slice, event.freq_slice,
+                                            "%s_%.3fMHz" % (event.shape, event_freq),
+                                            event.sig)
+                    relabeled_events.append(relabeled_event)
+                # Need to pull out the unique frequencies that were identified
+                new_event_shapes = []
+                for event in relabeled_events:
+                    _ind = event.shape.rfind("_")
+                    if event.shape[_ind + 1:] not in new_event_shapes:
+                        new_event_shapes.append(event.shape[_ind + 1:])
+                # need to iterate over unique frequencies
+                for subshape in new_event_shapes:
+                    subshape_key = f"narrow_{subshape}"
+                    shape_set = set()
+                    subshape_relevant_events = [event for event in relabeled_events if subshape in event.shape]
+                    for event in subshape_relevant_events:
+                        shape_set = shape_set.union(time_range[event.time_slice])
+                    occ_dict[subshape_key] = len(shape_set) / (ins.Ntimes - num_int_flag)
+                    if occ_dict[subshape_key] > 1:
+                        occ_dict[subshape_key] = 1
+        else:
+            for event in relevant_events:
+                shape_set = shape_set.union(time_range[event.time_slice])
+            occ_dict[shape] = len(shape_set) / (ins.Ntimes - num_int_flag)
+            if occ_dict[shape] > 1:
+                occ_dict[shape] = 1
+
+    if not lump_narrow:
+        occ_dict.pop("narrow")
+
     occ_dict['total'] = total_occ
 
     return(occ_dict)
