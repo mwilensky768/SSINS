ef80b3ff71811e25de5bfea16c8a4a1bd0c6213f
Mike Wilensky <mjw768@uw.edu>
2020-08-18 13:43:51 -0700

    update dependencies and version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = u'2018, Michael Wilensky'
 author = u'Michael Wilensky'
 
 # The short X.Y version
-version = u'1.4.2'
+version = u'1.4.3'
 # The full version, including alpha/beta/rc tags
 release = u''
 
