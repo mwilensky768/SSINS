550bc42e4368b5fe01eed838d7c6405c34852ea6
Mike Wilensky <mjw768@uw.edu>
2021-03-04 16:53:27 -0800

    Update version number

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = u'2018, Michael Wilensky'
 author = u'Michael Wilensky'
 
 # The short X.Y version
-version = u'1.4.4u'
+version = u'1.4.5'
 # The full version, including alpha/beta/rc tags
 release = u''
 
