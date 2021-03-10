f9f2aea892fb8b9d99bf12f206db48142adb247a
Mike Wilensky <mjw768@uw.edu>
2021-03-09 17:53:39 -0800

    use pyuvdata branch_scheme

--- SSINS/__init__.py
+++ SSINS/__init__.py
@@ -8,11 +8,13 @@ import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
-from . import version
 from .incoherent_noise_spectrum import *
 from .plot_lib import *
 from .match_filter import *
 from .sky_subtract import *
 from setuptools_scm import get_version
+from pathlib import Path
+from pyuvdata.branch_scheme import branch_scheme
 
-__version__ = get_version()
+version_str = get_version(Path(__file__).parent.parent, local_scheme=branch_scheme)
+__version__ = version_str
