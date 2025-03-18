84970c463d84888c78d52090ba2bef2a104eda7f
Mike Wilensky <wilensky.michael@gmail.com>
2025-03-18 17:54:44 -0400

    Add myst parser as an extension

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
